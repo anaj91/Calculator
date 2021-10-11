6f84b977f73b20e110169cfbb55efd9294594e43
Ana Jenko <ana.jenko91@gmail.com>
2021-10-11 16:10:58 +0200

    initial commmit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Lekcija 6 (3. domača naloga)
+
+print ("Welcome to my simple calculator! You can try simple calculating operations.")
+
+a = int(input("Add your first number:"))
+b = int(input("Add your second number:"))
+operation = input("Add operation (pick between + - * /)")
+
+if operation == "+":
+    print (a+b)
+elif operation == "-":
+    print (a-b)
+elif operation == "*":
+    print (a*b)
+elif operation == "/":
+    print (a/b)
+else: print ("Wrong input")
